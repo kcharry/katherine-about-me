c520f8bd41049774c08ee97a91deb314eccafc46
kcharry <kathycharry27@gmail.com>
2025-12-13 14:06:59 -0800

    Added a profile picture

--- app/pages/HomePage.tsx
+++ app/pages/HomePage.tsx
@@ -3,16 +3,21 @@ import './HomePage.css';
 export default function HomePage() {
   return (
     <div className="home-page">
-        <div className="about-me">
-            <h1>Katherine Charry</h1>
-            <h2>Hello World!</h2>
-            <p>I’m a second-year Computer Science student at the University of California, San Diego. 
-                I work in the Boolean Lab under Professor Sahoo, developing a web-based tool to automate 
-                primer design for miRNA detection, and contribute as a developer on the CSES Runway Avenue 
-                site development team. Beyond programming, I mentor first-year Computing students as a Lead 
-                Peer Mentor with CSE-PACE and organize and lead programming workshops for local high schools 
-                as the Logistics Lead for the ACM Outreach team.</p>
+        <div className="content">
+            <div className="about-me">
+                <h1>Katherine Charry</h1>
+                <h2>Hello World!</h2>
+                <p>I’m a second-year Computer Science student at the University of California, San Diego. 
+                    I work in the Boolean Lab under Professor Sahoo, developing a web-based tool to automate 
+                    primer design for miRNA detection, and contribute as a developer on the CSES Runway Avenue 
+                    site development team. Beyond programming, I mentor first-year Computing students as a Lead 
+                    Peer Mentor with CSE-PACE and organize and lead programming workshops for local high schools 
+                    as the Logistics Lead for the ACM Outreach team.</p>
+            </div>
+
+            <img className="profile-pic" src="/imgs/profile-pic.JPG" alt="Profile Picture" />
         </div>
+        
 
     </div>
   );
